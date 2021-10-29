38d813191d4433c2ca61dc0581ba8e150468c21d
Jaehwan Choi <lieblichoi@gmail.com>
2021-10-29 23:33:48 +0900

    [edit] 모임 만들기 apis 수정

--- src/shared/Axios.js
+++ src/shared/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://localhost:4000/",
+  baseURL: "http://3.36.95.193/",
   headers: {
     "content-type": "application/json;charset=UTF-8",
     accept: "application/json",
@@ -18,7 +18,7 @@ export const apis = {
   // 게시물 불러오기
   getPost: () => instance.get("/posts"),
   // 게시물 작성하기
-  createPost: (contents) => instance.post("/posts", contents),
+  addPost: (contents) => instance.post("/posts", contents),
   // 게시물 수정하기
   editPost: (id, content) => instance.put(`/posts/${id}`, content),
   // 게시물 삭제하기
