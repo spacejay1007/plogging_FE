584b3c4d076f28c76accc09023cf8688170490c4
jeoungwoo <ajw2312@gmail.com>
2021-10-30 17:07:35 +0900

    컴플릭

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
-// ConnectedRouter가 작동하지 않아 일시적으로 BrowserRouter 사용
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from '../redux/configureStore';
 
