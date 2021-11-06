2b92a796909dec58b42e3d120dafcf1a836e5229
Jaehwan Choi <lieblichoi@gmail.com>
2021-11-06 13:52:12 +0900

    [feat] GlobalStyles 적용 시도

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import "./index.css";
 import App from "./shared/App";
 import reportWebVitals from "./reportWebVitals";
 
+import GlobalStyle from "./styles/GlobalStyles";
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <GlobalStyle> */}
+        <App />
+      {/* </GlobalStyle> */}
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 reportWebVitals();
