bf4181a280eaedb88e28143a13c3f2426e70c4aa
Jaehwan Choi <lieblichoi@gmail.com>
2021-11-06 12:37:13 +0900

    [fix] 오타 수정

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -54,7 +54,7 @@ const PostCard = (props) => {
                 width="250px"
                 bold
                 size="20px"
-                textOverflo="ellipsis"
+                textOverflow="ellipsis"
                 whiteSpace="nowrap"
                 overFlow="hidden"
                 display="block"
