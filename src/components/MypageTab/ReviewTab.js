c42d24a657406c9d4b5c9b506964439dd871aa70
jeoungwoo <ajw2312@gmail.com>
2021-11-25 06:47:21 +0900

    edit: mypage(userImg), editProfile

--- src/components/MypageTab/ReviewTab.js
+++ src/components/MypageTab/ReviewTab.js
@@ -100,12 +100,14 @@ const ReviewTab = (props) => {
                 </Grid>
               </Grid>
               <Grid flexLeft padding='0'>
-                <>
+                {props.userImg === null ? (
                   <Image
                     shape='circle'
                     src='https://jupgging-image.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%B5%E1%84%87%E1%85%A9%E1%86%AB+%E1%84%91%E1%85%B3%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%AF+%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.jpg'
                   />
-                </>
+                ) : (
+                  <Image shape='circle' src={props.userImg} />
+                )}
                 <Text size='18px' color='#ACACAC' margin='0 0 0 5px'>
                   {props.nickname} 의 모임
                 </Text>
