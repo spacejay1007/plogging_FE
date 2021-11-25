200cd54280afda00ed3337ec01410254f01abfbc
spacejay1007 <spacejay1007@gmail.com>
2021-11-25 15:57:57 +0900

    edit:myBookmark

--- src/components/MypageTab/MeetingMyForm.js
+++ src/components/MypageTab/MeetingMyForm.js
@@ -11,41 +11,52 @@ const MeetingMyForm = (props) => {
   const dispatch = useDispatch();
 
   const crewList = useSelector((state) => state.crew.crew);
+  const users = useSelector((state) => state.user.userData?.data[0]);
+  console.log(users);
+
+  const badge = useSelector((state) => state.user.myBadge?.data);
+  console.log(badge);
+
+  const mypageNum = useSelector((state) => state.user.mypageNum?.data);
+  console.log(mypageNum);
   // console.log(crew_list);
 
   const crew_list = crewList?.slice(0).reverse();
   React.useEffect(() => {
     dispatch(crewActions.crewDB());
+    dispatch(userCreators.getUserDB());
+    dispatch(userCreators.getMyBadgeDB());
+    dispatch(userCreators.getMyPageNumDB());
   }, []);
 
   return (
     <React.Fragment>
       <Container>
-        <Grid center width='330px' margin='auto'>
-          <Grid mainFlex justifyContent='center' padding='0 0 10px 0'>
+        <Grid center width="330px" margin="auto">
+          <Grid mainFlex justifyContent="center" padding="0 0 10px 0">
             <>
               <Image
-                shape='circle'
-                size='150'
-                src='https://jupgging-image.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%B5%E1%84%87%E1%85%A9%E1%86%AB+%E1%84%91%E1%85%B3%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%AF+%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.jpg'
+                shape="circle"
+                size="150"
+                src="https://jupgging-image.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%B5%E1%84%87%E1%85%A9%E1%86%AB+%E1%84%91%E1%85%B3%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%AF+%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.jpg"
               />
             </>
           </Grid>
-          <Text size='24px' padding='10px 0 10px 0' bold>
+          <Text size="24px" padding="10px 0 10px 0" bold>
             {window.localStorage.getItem('nickname')}
           </Text>
-          <Grid margin='10px auto 40px auto'>
+          <Grid margin="10px auto 40px auto">
             <Tags large>{window.localStorage.getItem('email')}</Tags>
           </Grid>
-          <Grid center padding='0 0 120px 0'>
+          <Grid center padding="0 0 120px 0">
             <Buttons
               small
-              size='18px'
-              width='130px'
-              height='50px'
-              color='#fff'
-              bgColor='#333333'
-              borderRadius='10px'
+              size="18px"
+              width="130px"
+              height="50px"
+              color="#fff"
+              bgColor="#333333"
+              borderRadius="10px"
               _onClick={() => {
                 dispatch(userCreators.logOutMiddleware());
               }}
@@ -54,14 +65,89 @@ const MeetingMyForm = (props) => {
             </Buttons>
           </Grid>
         </Grid>
-        <Grid isFlex width='969px' height='44px' margin='0 auto 100px auto'>
+        <Grid
+          isFlex
+          width="968px"
+          height="202px"
+          border="1px solid #F8F8F8"
+          borderRadius="10px"
+          bg="#F8F8F8"
+          margin="0 auto 80px auto"
+        >
+          <Grid
+            columnFlex
+            width="242px"
+            height="150px"
+            borderRight="1px solid #D3D3D3"
+          >
+            <Text padding="0 0 15px 0">내 참여내역</Text>
+            <Grid alignEnd>
+              <Text size="27px" align="center" color="#23c8af" bold>
+                {mypageNum?.myCrews}
+              </Text>
+              <Text align="center" color="#23c8af">
+                건
+              </Text>
+            </Grid>
+          </Grid>
+          <Grid
+            columnFlex
+            width="242px"
+            height="150px"
+            borderRight="1px solid #D3D3D3"
+          >
+            <Text padding="0 0 15px 0">내 북마크</Text>
+            <Grid alignEnd>
+              <Text size="27px" align="center" color="#23c8af" bold>
+                {mypageNum?.myBookmarks}
+              </Text>
+              <Text align="center" color="#23c8af">
+                건
+              </Text>
+            </Grid>
+          </Grid>
+          <Grid
+            columnFlex
+            width="242px"
+            height="150px"
+            borderRight="1px solid #D3D3D3"
+          >
+            <Text padding="0 0 15px 0">내 후기</Text>
+            <Grid alignEnd>
+              <Text size="27px" align="center" color="#23c8af" bold>
+                {mypageNum?.myReivews}
+              </Text>
+              <Text align="center" color="#23c8af">
+                건
+              </Text>
+            </Grid>
+          </Grid>
+          <Grid
+            columnFlex
+            width="242px"
+            height="150px"
+            borderRight="1px solid #F8F8F8"
+          >
+            <Text padding="0 0 15px 0">획득 배지</Text>
+            <Grid alignEnd>
+              <Text size="27px" align="center" color="#23c8af" bold>
+                {mypageNum?.myBadges}
+              </Text>
+              <Text align="center" color="#23c8af">
+                개
+              </Text>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid isFlex width="969px" height="44px" margin="0 auto 100px auto">
           <Text
-            align='center'
-            width='242px'
-            height='44px'
-            color='#DBDCDB'
-            borderBottom='2px solid #DBDCDB'
-            cursor='pointer'
+            align="center"
+            width="242px"
+            height="44px"
+            color="#DBDCDB"
+            borderBottom="2px solid #DBDCDB"
+            cursor="pointer"
             _onClick={() => {
               history.push('/my');
             }}
@@ -69,12 +155,12 @@ const MeetingMyForm = (props) => {
             내 프로필
           </Text>
           <Text
-            align='center'
-            width='242px'
-            height='44px'
-            color='#DBDCDB'
-            borderBottom='2px solid #DBDCDB'
-            cursor='pointer'
+            align="center"
+            width="242px"
+            height="44px"
+            color="#DBDCDB"
+            borderBottom="2px solid #DBDCDB"
+            cursor="pointer"
             _onClick={() => {
               history.push('/crews/my');
             }}
@@ -82,12 +168,12 @@ const MeetingMyForm = (props) => {
             신청 내역
           </Text>
           <Text
-            align='center'
-            width='242px'
-            height='44px'
-            color='#DBDCDB'
-            borderBottom='2px solid #DBDCDB'
-            cursor='pointer'
+            align="center"
+            width="242px"
+            height="44px"
+            color="#DBDCDB"
+            borderBottom="2px solid #DBDCDB"
+            cursor="pointer"
             _onClick={() => {
               history.push('/bookmark/my');
             }}
@@ -95,12 +181,12 @@ const MeetingMyForm = (props) => {
             북마크
           </Text>
           <Text
-            align='center'
-            width='242px'
-            height='44px'
-            color='#DBDCDB'
-            borderBottom='2px solid #DBDCDB'
-            cursor='pointer'
+            align="center"
+            width="242px"
+            height="44px"
+            color="#DBDCDB"
+            borderBottom="2px solid #DBDCDB"
+            cursor="pointer"
             _onClick={() => {
               history.push('/reviews/my');
             }}
@@ -108,11 +194,11 @@ const MeetingMyForm = (props) => {
             후기 내역
           </Text>
           <Text
-            align='center'
-            width='242px'
-            height='44px'
-            borderBottom='2px solid #212121'
-            cursor='pointer'
+            align="center"
+            width="242px"
+            height="44px"
+            borderBottom="2px solid #212121"
+            cursor="pointer"
             _onClick={() => {
               history.push('/meeting/my');
             }}
@@ -121,10 +207,10 @@ const MeetingMyForm = (props) => {
           </Text>
         </Grid>
         <Grid>
-          <Text align='center' color='#666666' margin='0px 0px 5px 0px'>
+          <Text align="center" color="#666666" margin="0px 0px 5px 0px">
             모임 날짜 및 모집 기간, 모임 인원은 수정할 수 없습니다.
           </Text>
-          <Text align='center' color='#666666'>
+          <Text align="center" color="#666666">
             모임 날짜, 모집 기간, 모임 인원을 수정하고 싶으시다면 모임을 삭제한
             후 새로 모임을 만들어주세요.
           </Text>
