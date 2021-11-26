3f1ec83b2b99b4fb539a37717486e167ad102add
Jaehwan Choi <79303416+lieblichoi@users.noreply.github.com>
2021-11-26 20:25:22 +0900

    Update useSearchParams.js

--- src/shared/useSearchParams.js
+++ src/shared/useSearchParams.js
@@ -29,7 +29,8 @@ function getSearchParams(parsedSearchParams = {}, options = {}) {
   });
 }
 
-function useSearchParams() {
+// function useSearchParams() {
+const useSearchParams = () => {
   
   const { search } = useLocation();
   
